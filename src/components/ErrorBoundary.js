1d6d12a30e657a111e2b674cef686f2708cd67b0
peluboy <imulep2104@gmail.com>
2023-06-20 16:32:26 +0100

    worked on the styling

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,45 +1,41 @@
-import { Component } from 'react'
-import '../pages/error.css'
-
+import { Component } from "react";
+import "../pages/error.css";
 
 export class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false, error: null }
-      }
-    
-      static getDerivedStateFromError(error) {
-        return { hasError: true, error }
-      }
-    
-      componentDidCatch(error, errorInfo) {
-        // logErrorToMyService(error, errorInfo);
-        console.log("logging", error, errorInfo)
-      }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
 
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
 
-render() {
-const refreshPage = ()=>{
-    window.location.reload();
-}
+  componentDidCatch(error, errorInfo) {
+    // logErrorToMyService(error, errorInfo);
+    console.log("logging", error, errorInfo);
+  }
 
-if (this.state.hasError) {
+  render() {
+    const refreshPage = () => {
+      window.location.reload();
+    };
 
-return( 
-    <>
-        <div className='errorContainer'>
-            <div className='text'>
-                <h3>Something went Wrong</h3>
-                <button onClick={refreshPage}>Click to Reload</button>
+    if (this.state.hasError) {
+      return (
+        <>
+          <div className="errorContainer">
+            <div className="text">
+              <h3>An Error Occurred</h3>
+              <button onClick={refreshPage}>Click to Reload</button>
             </div>
             <div className="error">
-                <img src="./images/errorpage.svg" alt=""/>
+              <img src="./images/errorpage.svg" alt="" />
             </div>
-        </div>
+          </div>
         </>
-    )
-}
-    return this.props.children
-
+      );
     }
+    return this.props.children;
+  }
 }
--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -1,70 +1,83 @@
-import React, { useReducer } from 'react'
-import './counter.css'
-
+import React, { useReducer } from "react";
+import "./counter.css";
 
 const initialState = {
-  firstCount: 0
-}
-
+  firstCount: 0,
+};
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "increment":
-            return { firstCount: state.firstCount + action.value }
-        case "decrement":
-            return { firstCount: state.firstCount - action.value }
-        case "reset":
-            return initialState
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case "increment":
+      return { firstCount: state.firstCount + action.value };
+    case "decrement":
+      return { firstCount: state.firstCount - action.value };
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
-export const UseReducer = ({active, setActive}) => {
-    const [setCount, dispatch] = useReducer(reducer, initialState)
-    const changeCounterValue = (value)  => {
-      setCount(parseInt(value))
-    }
-    
-    
+export const UseReducer = ({ active, setActive }) => {
+  const [setCount, dispatch] = useReducer(reducer, initialState);
+  const changeCounterValue = (value) => {
+    setCount(parseInt(value));
+  };
+
   return (
-    <div className='mainContainer'>
+    <div className="mainContainer">
       <h3>Counter app with UseReducer</h3>
-      <div className='caption'>
-        <p>Subscribe  $10 to talk to a Therapist Today!</p>
+      <div className="caption">
+        <p>Subscribe $10 to talk to a Therapist Today!</p>
         <img src="./images/Mask.svg" alt="" />
       </div>
-        <div className='number'>
-          <img src="./images/dollar.svg" alt="" />
+      <div className="number">
+        <img src="./images/dollar.svg" alt="" />
 
-          <input type="number" placeholder='Enter value' onKeyUp={(event) => {
-          if(event.key === 'Enter')
-            changeCounterValue(event.target.value)
-        }}>
-
-        </input>
-          <div className='input'>
-            <h2 className={setCount.firstCount > 0 ? "positive" : setCount.firstCount < 0 ?"negative" : null}>{setCount.firstCount}</h2>
+        <input
+          type="number"
+          placeholder="Enter value"
+          onKeyUp={(event) => {
+            if (event.key === "Enter") changeCounterValue(event.target.value);
+          }}
+        ></input>
+        <div className="input">
+          <h2
+            className={
+              setCount.firstCount > 0
+                ? "positive"
+                : setCount.firstCount < 0
+                ? "negative"
+                : null
+            }
+          >
+            {setCount.firstCount}
+          </h2>
+        </div>
+        <div className="btns">
+          <div className="add">
+            <button onClick={() => dispatch({ type: "increment", value: 2 })}>
+              +
+            </button>
           </div>
-          <div className='btns'>
-            <div className='add'>
-              <button onClick={() => dispatch({type: 'increment', value:2})}>+</button>
-            </div>
-            <div className='subtract'>
-              <button onClick={() => dispatch({type: 'decrement', value:2})}>-</button>
-            </div>
+          <div className="subtract">
+            <button onClick={() => dispatch({ type: "decrement", value: 2 })}>
+              -
+            </button>
           </div>
         </div>
-        <div className='bottom'>
-          <div className='counter2'>
-            <div onClick={()=>setActive(!active)}>Click me!</div>
-          </div>
-          <div className='reset'>
-            <button onClick={() => dispatch({type: 'reset'})}>
-          <img src="./images/reload.svg" alt="" />
-              Reset</button>
-          </div>
+      </div>
+      <div className="bottom">
+        <div className="counter2">
+          <div onClick={() => setActive(!active)}>Click me!</div>
         </div>
+        <div className="reset">
+          <button onClick={() => dispatch({ type: "reset" })}>
+            <img src="./images/reload.svg" alt="" />
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
